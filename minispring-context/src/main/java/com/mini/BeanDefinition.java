0a8c129b3963fcac235622a5bf6cd63e3526f5b5
funnyx <329284843@qq.com>
2023-03-17 16:51:39 +0800

    commit

--- minispring-context/src/main/java/com/mini/BeanDefinition.java
+++
@@ -1,8 +0,0 @@
-package com.mini;
-
-/**
- * @author fanxiao  2023/3/17
- * @since 1.0.0
- */
-public class BeanDefinition {
-}
---
+++ minispring-context/src/main/java/com/mini/context/ClassPathXmlApplicationContext.java
@@ -0,0 +1,7 @@
+package com.mini.context;
+
+/**
+ * @author fanxiao 2023/3/17
+ * @since 1.0.0
+ */
+public class ClassPathXmlApplicationContext {}
