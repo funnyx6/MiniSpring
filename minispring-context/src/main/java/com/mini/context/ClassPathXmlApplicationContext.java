9d541911b5dcc47fb336777b36b076683e598efa
funnyx <329284843@qq.com>
2023-03-31 15:38:03 +0800

    commit

--- minispring-context/src/main/java/com/mini/context/ClassPathXmlApplicationContext.java
+++ minispring-context/src/main/java/com/mini/context/ClassPathXmlApplicationContext.java
@@ -8,6 +8,12 @@ import com.mini.core.ClassPathXmlResource;
 import com.mini.core.Resource;
 
 /**
+ * 解析 XML 文件中的内容。
+ *
+ * <p>加载解析的内容， 构建 BeanDefinition。
+ *
+ * <p>读取 BeanDefinition 的配置信息，实例化 Bean，然后把它注入到 BeanFactory 容器中。
+ *
  * @author fanxiao 2023/3/17
  * @since 1.0.0
  */
